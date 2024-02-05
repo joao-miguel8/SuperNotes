3c5911da5e844557cb6ac131d07ecd02f70fb39c
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-02-05 15:05:57 -0500

    - Add Tooltip to add new note icon

--- src/pages/AllNotes.tsx
+++ src/pages/AllNotes.tsx
@@ -2,6 +2,7 @@ import { IoIosMenu } from "react-icons/io";
 import { RxPencil2 } from "react-icons/rx";
 import { IoSearch } from "react-icons/io5";
 import { Icon } from "@chakra-ui/react";
+import { Tooltip } from "@chakra-ui/react";
 import SideNav from "../components/SideNav";
 
 function AllNotes() {
@@ -10,7 +11,7 @@ function AllNotes() {
 			<SideNav />
 			{/* notes snippets list container */}
 			<section className="w-80 inline-block border-[1.6px]   border-[#292F33] bg-[#171C1F]">
-				<div className="bg-red-200 h-full">
+				<div className="h-full">
 					{/* header + search bar wrapper */}
 					<div className="sticky top-0">
 						{/* header section (menu btn + title + add Note btn) */}
@@ -19,9 +20,16 @@ function AllNotes() {
 								<Icon as={IoIosMenu} color={"#ADB0B1"} boxSize={24} />
 							</button>
 							<span className=" text-white">All Notes</span>
-							<button>
-								<Icon as={RxPencil2} color={"#ADB0B1"} boxSize={24} />
-							</button>
+							<Tooltip
+								className="py-1 px-2 text-14 text-white bg-[rgba(68,67,67,0.16)] rounded-[16px] [box-shadow:0_4px_30px_rgba(0,_0,_0,_0.1)] backdrop-filter backdrop-blur-[11.6px] border-[1px] border-[rgba(61,60,60,0.42)]"
+								arrowSize={15}
+								hasArrow
+								label="Add a new note"
+								aria-aria-label="Add a new note tooltip">
+								<button>
+									<Icon as={RxPencil2} color={"#ADB0B1"} boxSize={24} />
+								</button>
+							</Tooltip>
 						</div>
 						{/* Search bar container */}
 						<div className="flex items-center bg-[#1A1F23]">
