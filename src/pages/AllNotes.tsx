a6291404045b9c232ff64bdb87a52ac6a5bec9ab
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-02-05 14:02:50 -0500

    :sparkles: Add notes snippets list container section markup/styling

--- src/pages/AllNotes.tsx
+++ src/pages/AllNotes.tsx
@@ -1,3 +1,7 @@
+import { IoIosMenu } from "react-icons/io";
+import { RxPencil2 } from "react-icons/rx";
+import { IoSearch } from "react-icons/io5";
+import { Icon } from "@chakra-ui/react";
 import SideNav from "../components/SideNav";
 
 function AllNotes() {
@@ -5,7 +9,29 @@ function AllNotes() {
 		<div className="inline-flex">
 			<SideNav />
 			{/* notes snippets list container */}
-			<div className="inline-block"></div>
+			<section className="w-80 inline-block border-[1.6px]   border-[#292F33] bg-[#171C1F]">
+				<div className="bg-red-200 h-full">
+					{/* header + search bar wrapper */}
+					<div className="sticky top-0">
+						{/* header section (menu btn + title + add Note btn) */}
+						<div className="p-[1rem] flex items-center justify-between bg-[#171C1F] border-b-[1.6px]   border-[#292F33]">
+							<button>
+								<Icon as={IoIosMenu} color={"#ADB0B1"} boxSize={24} />
+							</button>
+							<span className=" text-white">All Notes</span>
+							<button>
+								<Icon as={RxPencil2} color={"#ADB0B1"} boxSize={24} />
+							</button>
+						</div>
+						{/* Search bar container */}
+						<div className="flex items-center bg-[#1A1F23]">
+							<Icon as={IoSearch} boxSize={24} className="ml-4" color={"#6B6C70"} />
+							<input placeholder="Search all notes" type="text" className="py-4 px-2 w-full text-white bg-transparent outline-none placeholder:text-[#6B6C70]" />
+						</div>
+					</div>
+					<div className="w-full h-screen scrollbar-none overflow-auto overscroll-contain bg-[#171C1F]"></div>
+				</div>
+			</section>
 		</div>
 	);
 }
