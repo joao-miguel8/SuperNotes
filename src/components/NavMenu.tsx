b290507ce6d2d096e1aeae42421249e2de4b30fe
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-02-06 10:43:13 -0500

    - Add overflow-clip for parent container Add hover for nav items

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -27,19 +27,16 @@ function NavMenu() {
 
 	return (
 		// side menu wrapper
-		<section className="p-2 h-fit max-w-60 w-40 top-8 left-6 absolute z-40 bg-[#1D2327] rounded-lg">
+		<section className="h-fit max-w-60 w-40 top-8 left-6 absolute z-40 bg-[#1D2327] rounded-lg overflow-clip">
 			{/* side menu container */}
-			<div className="pt-1 max-w-60 h-full">
-				{/* divider */}
-				<div className="w-full h-[1.6px] bg-[#292F33]"></div>
+			<div className="max-w-60 h-full">
 				{/* nav list  */}
 				<nav className="flex flex-col h-full">
 					<ul className="mt-4 flex flex-col text-[#F6F9F8]">
 						{navigationMenu.map((navItem: NavigationMenuItemType) => {
 							const currentlyChosenMenuItem = navItem.path === currentPage;
-
 							return (
-								<li key={navItem.title} className={classNames(` items-center gap-2 font-semibold rounded-md ${currentlyChosenMenuItem && "bg-[#353E43]"}`)}>
+								<li key={navItem.title} className={classNames(`items-center gap-2 font-semibold hover:bg-[#353E43] ${currentlyChosenMenuItem && "bg-[#353E43]"}`)}>
 									<Link className="py-2 flex items-center h-full w-full rounded-md" to={navItem.path}>
 										<Icon className={classNames(`ml-2 ${currentlyChosenMenuItem && "text-white"}`)} as={navItem.icon} color={currentlyChosenMenuItem ? "text-white" : "#ADB0B1"} boxSize={14} />
 										<span className={classNames(`w-20 break-words ml-2 text-12 text-[#ADB0B1] ${currentlyChosenMenuItem && "text-white"}`)}>{navItem.title}</span>
