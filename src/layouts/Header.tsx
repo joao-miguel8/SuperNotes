7c19ebdd53b5425c329b298cdc2f3efe1ceb39da
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-02-06 18:45:45 -0500

    - Change styling in Header.tsx
    - Change padding, and Remove margin in header ele
    - Change Icon boxSize prop

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -4,10 +4,10 @@ import { Icon } from "@chakra-ui/react";
 
 function Header({ isSideNavClosed, setIsSideNavClosed }: { isSideNavClosed: boolean; setIsSideNavClosed: () => void }) {
 	return (
-		<header className="p-2 mx-2 sticky z-40 top-0">
+		<header className="p-3 sticky z-40 top-0 bg-[#1D2327]">
 			<div className="relative">
 				<button onClick={() => setIsSideNavClosed()}>
-					<Icon as={IoIosMenu} color={"#ADB0B1"} boxSize={24} />
+					<Icon as={IoIosMenu} color={"#ADB0B1"} boxSize={6} />
 				</button>
 				{/* menu */}
 				{isSideNavClosed && <NavMenu />}
