a86659434b39eff26d4379dccf885aec79e7ad5b
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-02-06 18:30:06 -0500

    - Remove ChakraProvider from App.tsx and wrap RouterProvider in main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import "./App.css";
-// import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
-	return <>{/* <ChakraProvider></ChakraProvider> */}</>;
+	return <></>;
 }
 
 export default App;
