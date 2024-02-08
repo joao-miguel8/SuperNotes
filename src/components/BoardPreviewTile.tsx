b7f7d55905e06b69cb68a8aa33b80d8763221bb8
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-02-08 09:47:14 -0500

    - Change text description text color

--- src/components/BoardPreviewTile.tsx
+++ src/components/BoardPreviewTile.tsx
@@ -6,7 +6,7 @@ function BoardPreviewTile({ boardPreviewData, currentBoardIndex, boards }: { boa
 	return (
 		<div className={classNames("px-4 p-2 rounded-lg hover:bg-[#353E43]", boards[currentBoardIndex]?.name === name && "bg-[#353E43]")}>
 			<h4 className="text-18 font-semibold truncate text-white">{name}</h4>
-			<p className="mt-2 text-14 text-[#0b0b12] line-clamp-2">{description}</p>
+			<p className="mt-2 text-14 text-[#A9ADAF] line-clamp-2">{description}</p>
 		</div>
 	);
 }
