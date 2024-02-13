0d996af05d93333d2709ea17681d5a77070cf122
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-02-13 15:06:20 -0500

    - Remove unnecessary Header css styling

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -2,11 +2,8 @@ import NavMenu from "../components/NavMenu";
 
 function Header() {
 	return (
-		<header className="sticky z-40 top-0 bg-[#1D2327]">
-			<div className="relative">
-				{/* menu */}
-				<NavMenu />
-			</div>
+		<header className=" bg-[#1D2327]">
+			<NavMenu />
 		</header>
 	);
 }
