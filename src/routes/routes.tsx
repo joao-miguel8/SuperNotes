a525f72d4c7bc25b7f580352f6ee773a04dfb233
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-02-14 18:33:26 -0500

    - Move Canvas page components
    - Create canvas pages subfolder
    - Create components folder in canvas page folder
    - Add all components associated to canvas page to components folder

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,7 +2,7 @@ import { Suspense, lazy } from "react";
 import { Navigate, createBrowserRouter } from "react-router-dom";
 
 const FlashCardsPage = lazy(() => import("../pages/flashcards/FlashCards"));
-const CanvasPage = lazy(() => import("../pages/Canvas"));
+const CanvasPage = lazy(() => import("../pages/canvas/Canvas"));
 const ErrorPage = lazy(() => import("../pages/Error"));
 
 export const router = createBrowserRouter([
