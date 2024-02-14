327f31f51e1a7499bdbe59ac7aec86423591764c
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-02-14 16:47:56 -0500

    Remove icon import

--- src/components/preview/PreviewListContainer.tsx
+++ src/components/preview/PreviewListContainer.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Icon, Tooltip } from "@chakra-ui/react";
 import classNames from "classnames";
-import { IoArrowBackOutline, IoSearch } from "react-icons/io5";
+import { IoArrowBackOutline } from "react-icons/io5";
 import { RxPencil2 } from "react-icons/rx";
 import DeckPreviewTile from "../deck/DeckPreviewTile";
 import type { DeckType } from "../../types/DeckType";
