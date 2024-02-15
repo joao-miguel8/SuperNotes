720af7ad9e8c3818e0f22f0469cf86dd1a417642
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-02-15 12:23:52 -0500

    - Add optional chain to PreviewDeckList

--- src/pages/flashcards/components/preview/PreviewDeckList.tsx
+++ src/pages/flashcards/components/preview/PreviewDeckList.tsx
@@ -4,7 +4,7 @@ import DeckPreviewTile from "../deck/DeckPreviewTile";
 function PreviewDeckList({ decks, currentDeckIndex, selectAndDeselectChosenDeck }: { decks: DeckType[]; currentDeckIndex: number; selectAndDeselectChosenDeck: (chosenIndex: number, index: number) => void }) {
 	return (
 		<ul className="mt-4 mx-4 flex flex-col gap-2">
-			{decks.map((deck, index) => {
+			{decks?.map((deck, index) => {
 				return (
 					<li onClick={() => selectAndDeselectChosenDeck(currentDeckIndex, index)} key={deck.name}>
 						<DeckPreviewTile deckPreviewData={deck} currentDeckIndex={currentDeckIndex} decks={decks} />
