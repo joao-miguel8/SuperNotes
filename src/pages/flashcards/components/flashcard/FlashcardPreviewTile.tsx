5d231692cf9cc7f5b480a28c33f22616e6262925
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-02-17 18:49:28 -0500

    - Fix flashcard selected preview tile conditional styling

--- src/pages/flashcards/components/flashcard/FlashcardPreviewTile.tsx
+++ src/pages/flashcards/components/flashcard/FlashcardPreviewTile.tsx
@@ -3,7 +3,7 @@ import type { FlashCardType } from "@/types/FlashCardType";
 
 function FlashcardPreviewTile({ currentFlashCard, flashcard }: { currentFlashCard: FlashCardType | null; flashcard: FlashCardType }) {
 	return (
-		<div className={classNames("group p-2 overflow-hidden rounded-lg bg-[#fff] duration-150 hover:bg-[#bfbfbf]", currentFlashCard === flashcard && "bg-[#bfbfbf]")}>
+		<div className={classNames("group p-2 overflow-hidden rounded-lg duration-150 hover:bg-[#bfbfbf]", currentFlashCard?.id === flashcard.id ? "bg-[#bfbfbf]" : "bg-[#fff]")}>
 			<h5 className="text-center text-14 italic font-semibold">Front:</h5>
 			<span className="mt-2 w-full inline-block text-center truncate text-14 text-[#1A1F23]">{flashcard.front}</span>
 		</div>
