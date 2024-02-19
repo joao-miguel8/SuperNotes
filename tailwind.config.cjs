939b15ff57336624c1db933b4432597ef34904ce
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-02-19 09:57:19 -0500

    - Remove ThemeProvider Remove and tailwind.config theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,15 +12,7 @@ module.exports = {
 	content: ["./src/**/*.{html,js,ts,tsx}"],
 	theme: {
 		fontSize: fontSizes,
-		extend: {
-			theme01: {
-				"color-textLight-theme01": "#212427",
-				"color-textDark-theme01": "#ffff",
-				"color-light100-theme01": "#9595FF",
-				"color-medium100-theme01": "#6A6AE3",
-				"color-dark100-theme01": "#4747BC",
-			},
-		},
+		extend: {},
 	},
 
 	plugins: [require("tailwind-scrollbar")],
